35805b74ffe1cd919a3ccd53e054283bd28a01be
Nastyshka <58233118+NastyaB1@users.noreply.github.com>
2020-11-24 21:56:32 +0200

    Create js2.js

---
+++ js2.js
@@ -0,0 +1,24 @@
+function f()
+{
+document.getElementById("pic1").src="2.gif";
+document.fgColor="blue";
+document.bgColor="yellow";
+}
+ 
+function g()
+{
+if(document.getElementById("sl1").selectedIndex==0)
+document.bgColor="green";
+ 
+if(document.getElementById("sl1").selectedIndex==1)
+document.bgColor="red";
+ 
+if(document.getElementById("sl1").selectedIndex==2)
+document.bgColor="pink";
+ 
+ 
+}
+function rr()
+{
+document.getElementById("area1").value="Привет, Мир!!!";
+ 
